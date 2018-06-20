2790fe42d1943f78c29e91dae1ba8766ea7816dc
willou <walid.boubguira@gmail.com>
2018-06-20 19:33:04 -0400

    initianl

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
 // Building up the ajax call
 
+
+
 // creating our queryUrl variables so we can target the right API when the inputs are entered
-var locationAjax = $("#location").val().trim();
+var locationAjax = $("#location").val().trim()
+// .replaceSpace.replace(" ", "+");
+// console.log(locationAjax);
 var categoryAjax = $("#category").val().trim();
 var keywordAjax = $("#keyword").val().trim();
 // the time needs to be entered in a specific format like (This+weekend or 29+December+2018) 
@@ -20,21 +24,25 @@ $.ajax({
 }).then(function(response1){
     console.log(queryURL)
     
+    var results = response1.events;
+
+    console.log(results);
     // The response will go here
 });
 
 
 // Initializing the firebase 
 
-var config = {
-    apiKey: "AIzaSyB-8hqysBzeElEDP5mjEFa2BpJsM3W8eTk",
-    authDomain: "project-happenin.firebaseapp.com",
-    databaseURL: "https://project-happenin.firebaseio.com",
-    projectId: "project-happenin",
-    storageBucket: "project-happenin.appspot.com",
-    messagingSenderId: "167007450235"
-  };
+// var config = {
+//     apiKey: "AIzaSyB-8hqysBzeElEDP5mjEFa2BpJsM3W8eTk",
+//     authDomain: "project-happenin.firebaseapp.com",
+//     databaseURL: "https://project-happenin.firebaseio.com",
+//     projectId: "project-happenin",
+//     storageBucket: "project-happenin.appspot.com",
+//     messagingSenderId: "167007450235"
+//   };
 
-  firebase.initializeApp(config);
+//   firebase.initializeApp(config);
 
-  var database = firebase.database();
+//   var database = firebase.database();
+
