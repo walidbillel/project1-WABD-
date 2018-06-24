27bc984ceb6837cb00825d0d4c0ac2d851e355e4
willou <walid.boubguira@gmail.com>
2018-06-23 20:09:11 -0400

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,29 @@
-//Testing Header Slideshow
-
-// Building up the ajax call
 
 
 
 
+// Event Handler for the first API that pulls the events when clicking the submit button
 $("#submit").on("click", function () {
+
 
   event.preventDefault();
 
+  // Creating a variable that holds the performers and then get the value entered
   var artistAjax = $("#performers").val();
+  // Replacing spaces with dashes so the callback works proprely
   artistAjax = artistAjax.split(" ").join("-");
   artistAjax = artistAjax.toLowerCase();
 
+  // Creating a variable that holds the city and then get the value entered
   var cityAjax = $("#where").val();
+  // Replacing spaces with dashes so the callback works proprely
   cityAjax = cityAjax.split(" ").join("-");
+  // changing it to lower case
   cityAjax = cityAjax.toLowerCase();
 
   // var dateAjax = $("#when").val();
 
+  // Creating a variable that holds the wordkey and then get the value enetered
   var keywordAjax = $("#keywords").val();
   keywordAjax = keywordAjax.split(" ").join("-");
   keywordAjax = keywordAjax.toLowerCase();
@@ -30,30 +35,38 @@ $("#submit").on("click", function () {
   // console.log(artistAjax);
   // console.log(cityAjax);
 
+
+  // Creating the callback queryURL based on the parameters eneterd
   var queryURL = "https://api.seatgeek.com/2/events?performers.slug=" + artistAjax + "&q=" + keywordAjax + "&venue.city=" + cityAjax + "&client_id=MTIwMDM0Mjl8MTUyOTUzNDYwOS42";
 
   // console.log(queryURL);
   // Performing the ajax request with the queryURL
 
 
-
+  // Creating the Ajax call for the first API seatgeek
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response1) {
     // console.log(queryURL);
+    // empty the result so each time it loads with new results and not putting them over each other
     $("#results").empty();
 
+    // creating a variable that holds the events
     var results = response1.events;
 
+    // looping over the results
     for (var i = 0; i < results.length; i++) {
 
+      // creating a div that holds the results
       var ourDiv = $("<div>");
 
+      // Getting the title from the ajax call then append it to the results
       var title = $("<h3>");
       title.append(results[i].title);
       ourDiv.append(title, "<hr>");
 
+      // getting the image from the call and appending it to the results
       var image = $("<img>");
       // console.log(results[i].performers[0].image);
       image.attr("src", results[i].performers[0].image);
@@ -61,11 +74,13 @@ $("#submit").on("click", function () {
       ourDiv.append(image);
 
 
+      // getting the date and time from the call and appending it to the results
       var dateAndTime = $("<p>");
       dateAndTime.append("<hr>", "<b>Date & Time: </b>" + results[i].datetime_local);
       ourDiv.append(dateAndTime);
 
 
+      // getting the venue name and appending it to the results
       var venueName = $("<p>");
       venueName.append("<b>Venue Name: </b>" + results[i].venue.name);
       ourDiv.append(venueName);
@@ -74,17 +89,19 @@ $("#submit").on("click", function () {
       venueAddress.append("<b>Venue Address: </b>" + results[i].venue.address + ", " + results[i].venue.extended_address);
       ourDiv.append(venueAddress);
 
+      // getting the prices and appending them to the results
       var pricesRange = $("<p>");
       pricesRange.append("Lowest Price: $" + results[i].stats.lowest_price, "<br>");
       pricesRange.append("average Price: $" + results[i].stats.average_price, "<br>");
       pricesRange.append("highest Price: $" + results[i].stats.highest_price);
       ourDiv.append(pricesRange);
 
+      // getting the ticket url and appending it to the results as an embeded link in a text
       var ticketUrl = $("<p>");
       ticketUrl.append("<a href=" + "'" + results[i].url + "'" + ">" + "<b> Get Your Ticket Now! </b>" + "</a>", "<hr>");
       ourDiv.append(ticketUrl);
 
-
+      // showing our results on the page
       $("#results").append(ourDiv);
     }
 
@@ -94,15 +111,19 @@ $("#submit").on("click", function () {
 });
 
 
-
+// Event Handler for the second API that pulls the directions when clicking the submit button
 $("#submit-direction").on("click", function () {
 
   event.preventDefault();
 
+  // creating a variable that holds the current location then getting the value enetered
   var currentLocaAjax2 = $("#current-location").val();
+  // replacing spaces with "+" so the ajax call works proprely
   currentLocaAjax2 = currentLocaAjax2.split(" ").join("+");
+  // Event Handler for the first API that pulls the events when clicking the submit button
   console.log(currentLocaAjax2);
 
+  // creating a variable that holds the destination then getting the value enetered
   var destinationAjax2 = $("#destination").val();
   destinationAjax2 = destinationAjax2.split(" ").join("+");
   console.log(destinationAjax2);
@@ -110,7 +131,7 @@ $("#submit-direction").on("click", function () {
 
 
 
-
+  //  Creating the second callback queryURL from mapquest based on the parameters eneterd
   queryURL = "http://www.mapquestapi.com/directions/v2/route?key=DZcQ6A8PzsQFGMHAqxissAvWHreJTluG&from=" + currentLocaAjax2 + "&to=" + destinationAjax2 + "&enhancedNarrative=true"
 
   $.ajax({
@@ -118,36 +139,37 @@ $("#submit-direction").on("click", function () {
     method: "GET"
   }).then(function (response2) {
     console.log(queryURL);
-    $("#results").empty();
 
+    // empty the result so each time it loads with new results and not putting them over each other
+    $("#direction-result").empty();
+
+    // creating a variable that holds the results
     var results = response2.route;
 
+    // creating a p tag
+    var totalDistance = $("<p>");
+    // getting the data for total distance from the ajax call
+    totalDistance = results.distance;
+    // Appending the information to the results (on webpage)
+    $("#direction-result").append(" <b> Total Distance: </b>" + totalDistance + " Miles. " + "<hr>");
 
-    var totalDistance = results.distance + " miles";
 
-    console.log("Total Distance is: " + totalDistance);
-    $("#direction-result").append("Total Distance: " + totalDistance);
-
+    // creating a variable that holds maneuvers and getting the data from the ajax call
     var maneuversDir = results.legs[0].maneuvers;
     // console.log(maneuversDir);
 
+    // looping over the array and getting information
     for (var i = 0; i < maneuversDir.length; i++) {
 
-
-      var narrativeDir = maneuversDir[i].narrative;
-      // console.log(narrativeDir)
-      var narrDistance = maneuversDir[i].distance;
-
-
-      step = narrativeDir + " For " + narrDistance + " miles, then =>";
-      console.log(step)
+      // create a list that will hold the directions
+      var narrativeDirection = $("<li>");
+      var narrativeDistance = maneuversDir[i].distance;
 
 
-      $("#direction-result").append(step);
+      // Appending our directions to the results (on webpage)
+      narrativeDirection.append(maneuversDir[i].narrative + " for: " + narrativeDistance + " Miles. " + "<hr>");
+      $("#direction-result").append(narrativeDirection);
 
-
-      // console.log(maneuversDir[i].narrative);
-      // console.log(maneuversDir[i].distance);
     }
 
 
